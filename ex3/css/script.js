ff7e54f7ae8f6c40af7742ce57c40a47e37b237f
idan kario <idankario@gmail.com>
2021-04-19 21:01:27 +0300

    update

--- ex3/css/script.js
+++ ex3/css/script.js
@@ -1,12 +1,12 @@
 
 // Global virable :
-
 const colors=["#363b3f", "#E14D43", "#26292c","#a3ca61","#ff6f6f","#fbcf61","#d97761","#0d4261","#00cc99","#006598",
 "#82b440","#428bca","#E77755","#523e7c","#f77e05","#24a828","#f2b91f","#94b998","#3CBD99","#019ad2","#e0e0e0","#ec3939"]
 let numE=0
-const S=colors.length-1
+const s=colors.length-1
+
 // get Random Num
-function getRandomNum (){return Math.floor(Math.random() * (S))  }
+function getRandomNum (){return Math.floor(Math.random() * (s))  }
 function createBox(wrapper){
 	let s= $(document.createElement("section")).css("background-color", colors[numE]);
 	$(wrapper).append(s);
@@ -25,7 +25,7 @@ function crateRecList(wrapper,r){
 //Add random color and pic for %3
 function addAtrBox(){
 	let s=$(".wrapper section").first();
-	s.html('<span class="fas fa-plus-circle"></span>');
+	s.html('<span class="plus" href="#"></span>');
 }
 
 function addRec(n){
@@ -36,14 +36,15 @@ function addRec(n){
 $(function() {
 	addRec("kario")
 });
+
 //On click tongle background-color/pic
 $(document).on("click", ".wrapper section:not(:first-child)", function(){
 	let q=this.style.backgroundColor == 'rgb(255, 255, 255)';
 	this.style.backgroundColor =  q ? colors[getRandomNum()] : 'rgb(255, 255, 255)';
 	if(!q)
-		$(this).addClass('img1').fadeTo(1000, 1);
+		$(this).addClass('imgBgr');
 	else
-		$(this).removeClass('img1').fadeTo(1000, 1);
+		$(this).removeClass('imgBgr');
 });
 //On click plus create new box
 $(document).on("click", ".wrapper span", function(){
