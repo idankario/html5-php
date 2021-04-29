ce1b7121ea9d197d7e395b29acee533d17258cf6
idan kario <idankario@gmail.com>
2021-04-29 19:10:34 +0300

    update

--- ex4/getItem.php
+++ ex4/getItem.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+      <title>idankario-ex4 php</title>
       <link rel="icon" href="images/icon.svg">
-      <meta charset="utf-8">
+      <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
       <!-- Bootstrap CSS -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
       <link rel = "stylesheet" href="css/style.css" >
-      <title>idankario-ex4 php</title>
 	</head>
 <body>
    <!-- container -->
@@ -16,7 +16,7 @@
     <section>
       <img src='images/love.svg' alt="love" title="love">
     </section>
-		<main class="c">
+		<section class="c">
       <header class="text-light">
         <h1>Our Product</h1>
       </header>
