7d5ea7c5adb3e848d167bf6b46f524d03afa2969
idan kario <idankario@gmail.com>
2021-05-01 19:53:21 +0300

    update

--- project/css/script.js
+++ project/css/script.js
@@ -1,7 +1,6 @@
 
 
 const sliderFunction=()=> {
-
 	let curSlide     = 0,
 	autoSlideTimeout = 9000
 	/**
@@ -243,8 +242,7 @@ const createFilter=()=> {
 		}
 	});
 }
-
-window.onload=()=>{
+$(()=> {
 	if($(".slider-container-header").length)
 	{
 		sliderFunction()
@@ -253,5 +251,69 @@ window.onload=()=>{
 		createProductAndFilter()
 		createFilter()
 	}
-}
-
+	if($(".main-form").length){
+		formValidations()
+	}
+  });
+  
+  // Submission notification to user
+  const constructValidation=(e,test)=>{
+	if (test) {
+	  if (!e.target.classList.contains('is-valid'))
+		e.target.classList.add('is-valid');
+	  e.target.className = e.target.className.replace(
+		/\bis-invalid\b/,
+		'is-valid'
+	  );
+	} else {
+	  if (!e.target.classList.contains('is-invalid'))
+		e.target.classList.add('is-invalid');
+	  e.target.className = e.target.className.replace(
+		/\bis-valid\b/,
+		'is-invalid'
+	  );
+	}
+  }
+  // Submission notification to user and check valid input
+  const formValidations = () => { 
+	window.addEventListener(
+	  'load',
+	  () => {   
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		let forms = document.getElementsByClassName('main-form');
+  
+		// Loop over them and prevent submission
+		Array.prototype.filter.call(forms, (form) => {
+		// Input Name one word or two word with one space
+		form[0].onkeyup = (e) => {
+		  const regex = /^[a-zA-Z]+\s[a-zA-Z]+$/
+		  const regex2 = /^[a-zA-Z]+$/;
+		  let test=regex.test(e.target.value)||regex2.test(e.target.value)
+		  constructValidation(e,test);
+		 
+		};
+		/*Password should Minimum eight characters, at least one letter and one number:
+		**/
+		form[1].onkeyup = (e) => {
+			const regex = /^[a-zA-Z]+\s[a-zA-Z]+$/
+			const regex2 = /^[a-zA-Z]+$/;
+			let test=regex.test(e.target.value)||regex2.test(e.target.value)
+			constructValidation(e,test);
+		};
+		form.addEventListener(
+		  'submit',
+		  (event) => {
+			if (form.checkValidity() === false) {
+			  console.log('not valid form');
+			  event.preventDefault();
+			  event.stopPropagation();
+			}
+			form.classList.add('was-validated');
+		  },
+		  false
+		  );
+		});
+	  },
+	);
+  };
+  
